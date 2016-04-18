166f6816abe700438911cf2526dbdb79e01efff2
Pietro Grandi <pietro.grandi@betsson.com>
2016-04-18 19:54:40 +0200

    built 1.8

--- bin/dts-generator.js
+++ bin/dts-generator.js
@@ -1,45 +1,57 @@
-var dts = require('../index');
-var kwArgs = { files: [] };
-for (var i = 2, j = process.argv.length; i < j; ++i) {
-    var arg = process.argv[i];
-    if (arg.charAt(0) === '-') {
-        var key = process.argv[i].replace(/^-+/, '');
-        var value = process.argv[i + 1];
-        ++i;
-        if (key === 'exclude') {
-            if (!kwArgs.excludes) {
-                kwArgs.excludes = [];
+"use strict";
+var index_1 = require('../index');
+module.exports = function main(argv) {
+    var kwArgs = {
+        files: [],
+        sendMessage: console.log.bind(console)
+    };
+    for (var i = 0; i < argv.length; ++i) {
+        var arg = argv[i];
+        if (arg.charAt(0) === '-') {
+            var key = argv[i].replace(/^-+/, '');
+            var value = argv[i + 1];
+            ++i;
+            if (key === 'exclude') {
+                if (!kwArgs.exclude) {
+                    kwArgs.exclude = [];
+                }
+                kwArgs.exclude.push(value);
             }
-            kwArgs.excludes.push(value);
-        }
-        else if (key === 'extern') {
-            if (!kwArgs.externs) {
-                kwArgs.externs = [];
+            else if (key === 'extern') {
+                if (!kwArgs.externs) {
+                    kwArgs.externs = [];
+                }
+                kwArgs.externs.push(value);
             }
-            kwArgs.externs.push(value);
+            else if (key === 'verbose') {
+                kwArgs.verbose = true;
+                /* decrement counter, because vebose does not take a value */
+                --i;
+            }
+            else {
+                kwArgs[key] = value;
+            }
         }
         else {
-            kwArgs[key] = value;
+            kwArgs.files.push(argv[i]);
         }
     }
-    else {
-        kwArgs.files.push(process.argv[i]);
-    }
-}
-['baseDir', 'name', 'out'].forEach(function (key) {
-    if (!kwArgs[key]) {
-        console.error('Missing required argument "' + key + '"');
+    ['out'].forEach(function (key) {
+        if (!kwArgs[key]) {
+            console.error("Missing required argument \"" + key + "\"");
+            process.exit(1);
+        }
+    });
+    if (!kwArgs.baseDir && !kwArgs.project) {
+        console.error("Missing required argument of \"baseDir\" or \"project\"");
         process.exit(1);
     }
-});
-if (kwArgs.files.length === 0) {
-    console.error('Missing files');
-    process.exit(1);
-}
-console.log('Starting');
-dts.generate(kwArgs, console.log.bind(console)).then(function () {
-    console.log('Done!');
-}, function (error) {
-    console.error(error);
-    process.exit(1);
-});
+    if (!kwArgs.project && kwArgs.files.length === 0) {
+        console.error('Missing files');
+        process.exit(1);
+    }
+    console.log('Starting');
+    return index_1.default(kwArgs).then(function () {
+        console.log('Done!');
+    });
+};
